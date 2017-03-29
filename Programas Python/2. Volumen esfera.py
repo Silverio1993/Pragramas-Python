cf24c05de14ce954720c18fa8bc96501d1a7a340
Silverio1993 <jesus_123_1993@hotmail.com>
2017-03-28 23:43:18 -0600

    Add files via upload

---
+++ Programas Python/2. Volumen esfera.py
@@ -0,0 +1,10 @@
+import math
+radio = input("Ingrese el radio de la esfera: ")
+
+def volumen(radio):
+	pass
+
+volumen = ( math.pi * radio**3)/3
+
+print("El volumen de la esfera es :", volumen)
+input()
---
+++ Programas Python/8. numerosentre.py
@@ -0,0 +1,9 @@
+def numeros(n):
+	pass
+
+print("Los numeros son:")
+
+for n in range(20, 60):
+		print (n)
+
+input()
